45e0acaa14896e721c8824bbbf3a8452a3c27212
nguyenquanghao2505-rgb <nguyenquanghao2505@gmail.com>
2026-08-22 21:34:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 window.SPORTSVN_CONFIG = {
-  supabaseUrl: 'https://bqzjksmsdtnodcfykggk.supabase.co',
-  supabasePublishableKey: 'sb_publishable_PyxvVa3K8LgTRyGCXeNLwA_HUoRPgli',
+  supabaseUrl: 'https://bzqjksmsdtnodcfykggk.supabase.co',
+
+  supabasePublishableKey: 'sb_publishable_Pyxv3k8LgTRyGCXenLWa_HUuRPgli',
+
   adminEmail: 'nguyenquanghao2505@gmail.com',
-  siteUrl: 'https://sportsvn.com',
-  contact: {
-    email: 'nguyenquanghao2505@gmail.com',
-    phone: '0905771177',
-    zalo: ['0905771177', '0384913999']
-  }
+
+  siteUrl: 'https://sportsvn.com'
 };
