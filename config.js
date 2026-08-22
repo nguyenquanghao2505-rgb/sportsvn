c33a68603021ec136626e41d2be62ad943c9d89a
nguyenquanghao2505-rgb <nguyenquanghao2505@gmail.com>
2026-08-22 22:29:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
 window.SPORTSVN_CONFIG = {
-  SUPABASE_URL: 'https://bzqjksmdtnodcfykggk.supabase.co',
-
-  SUPABASE_ANON_KEY: 'sb_publishable_Pyxv3k8LgTRyGCXenLWa_HUuRPgli',
-
-  ADMIN_EMAIL: 'nguyenquanghao2505@gmail.com',
-
-  SITE_URL: 'https://sportsvn.com'
+  SUPABASE_URL: "URL_SUPABASE_CUA_BAN",
+  SUPABASE_ANON_KEY: "ANON_KEY_CUA_BAN"
 };
