ca78e9d155093e9f3abbd89384b8e2e26aaa1342
nguyenquanghao2505-rgb <nguyenquanghao2505@gmail.com>
2026-08-24 16:32:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-// ======================================================
-// SPORTSVN - SUPABASE CONFIGURATION
-// ======================================================
-
 window.SPORTSVN_SUPABASE = {
-  url: "https://bqzjksmsdtnodcfykggk.supabase.co",
+  url: "https://bqzjksmsdtnodcfykgqgk.supabase.co",
   anonKey: "sb_publishable_PyxvVa3K8LgTRyGCXeNLwA_HUoRPgli"
 };
