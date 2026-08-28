4e538997a70bfb0132f94e9d979d4a5128d2c062
nguyenquanghao2505-rgb <nguyenquanghao2505@gmail.com>
2026-08-28 13:11:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    Chỉ dùng Publishable/Anon key, KHÔNG đưa Service Role key vào đây. */
 window.SPORTSVN_CONFIG = {
   SUPABASE_URL: "https://pejtrmugzyrflxmjblts.supabase.co",
-  SUPABASE_ANON_KEY: "DAN_PUBLISHABLE_ANON_KEY_CUA_SUPABASE_VAO_DAY"
+  SUPABASE_ANON_KEY: "sb_publishable_PyxvVa3K8LgTRyGCXeNLwA_HUoRPgli"
 };
